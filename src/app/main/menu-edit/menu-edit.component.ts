36246c93168765beba280f6a7963aa19da317e27
Mate, Galvacs <mate.galvacs@zeiss.com>
2023-04-29 17:36:26 +0200

    Create design for menu edit component

--- src/app/main/menu-edit/menu-edit.component.ts
+++ src/app/main/menu-edit/menu-edit.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { MenuDTO } from "../../shared/models/menu.model";
+import { DrinkGroupDTO } from "../../shared/models/drink-group.model";
+import { StoreService } from "../../shared/services/data/store.service";
 
 @Component({
   selector: 'app-menu-edit',
   templateUrl: './menu-edit.component.html',
   styleUrls: ['./menu-edit.component.scss']
 })
-export class MenuEditComponent {
+export class MenuEditComponent implements OnInit {
+  public menuList: MenuDTO[] = [];
+  public drinkList: DrinkGroupDTO[] = [];
+  constructor(private storageService:StoreService) {
+  }
+  ngOnInit() {
+    this.storageService.selectRestaurant().subscribe((restaurant)=> {
+      if (!restaurant){
+        this.menuList = [];
+        this.drinkList = [];
+        return;
+      }
+      this.menuList = restaurant.menu.sort(this.compare);
+      this.drinkList = restaurant.drinks;
+    })
+  }
 
+  compare( a: MenuDTO, b: MenuDTO ) {
+    if ( a.type < b.type ){
+      return -1;
+    }
+    if ( a.type > b.type ){
+      return 1;
+    }
+    return 0;
+  }
 }
