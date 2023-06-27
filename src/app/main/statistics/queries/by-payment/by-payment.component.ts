dbe4409d26fd14986847f449c7625d6902e529fe
HaloReach13 <juhaszbazsi13@gmail.com>
2023-06-27 17:25:34 +0200

    Create payment query

--- src/app/main/statistics/queries/by-payment/by-payment.component.ts
+++ src/app/main/statistics/queries/by-payment/by-payment.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Statistics } from "../../../../shared/models/order.model";
+import { PaymentQuery } from 'src/app/shared/models/payment-query.model';
+import { OrderItemDTO } from 'src/app/shared/models/order-item.model';
 
 @Component({
   selector: 'app-by-payment',
@@ -8,5 +10,21 @@ import { Statistics } from "../../../../shared/models/order.model";
 })
 export class ByPaymentComponent {
   @Input()
-  statistics : Statistics[] = []
+  statistics: Statistics[] = [];
+  statisticList: PaymentQuery[] = [];
+  items: OrderItemDTO[] = [];
+
+  ngOnInit() : void {
+    this.statistics.forEach(statistic => {
+      statistic.items.forEach(item => {
+        this.items.push(item);
+        const paymentQuery: PaymentQuery = {
+          paymentMethod : statistic.payWithCard,
+          count : 1,
+          amount : item.price
+        }
+        this.statisticList.push(paymentQuery);
+      })
+    })
+  }
 }
